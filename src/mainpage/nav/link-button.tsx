da6f5d2ae0c13e3f943fb1b8f158e9020e6d14b4
cindyhont <cindyhont@gmail.com>
2022-12-28 18:55:45 +0000

    .

--- src/mainpage/nav/link-button.tsx
+++ src/mainpage/nav/link-button.tsx
@@ -42,7 +42,7 @@ const LinkButton = (
                     top:window.matchMedia('(min-width:600px)').matches ? top - 50 : top,
                     behavior:'smooth'
                 })
-                setTimeout(()=>request.current = requestAnimationFrame(onScroll),200)
+                setTimeout(()=>request.current = requestAnimationFrame(onScroll),300)
                 
                 /*
                 if (mobile){
