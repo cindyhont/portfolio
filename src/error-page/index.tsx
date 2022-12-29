86e8c6a7ad3923dc074b7bad204932b27cc3d029
cindyhont <cindyhont@gmail.com>
2022-12-29 22:02:16 +0000

    rewrite scss

--- src/error-page/index.tsx
+++ src/error-page/index.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import Link from 'next/link'
-import styles from './ErrorPage.module.scss'
+import styles from './Errorpage.module.scss'
 
 const ErrorPage = ({errorCode}:{errorCode:number}) => (
     <main className={styles.errorpage}>
