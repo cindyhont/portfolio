6accb95b1394f07d071093dc8a91137df2eb47aa
cindyhont <cindyhont@gmail.com>
2023-01-09 07:44:23 +0000

    .

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,9 @@
 import React from "react"
 import ErrorPage from "../src/error-page"
 
+export const config = {
+    unstable_runtimeJS: false
+};
+
 const FourOFour = () => <ErrorPage errorCode={404} />
 export default FourOFour
--- pages/500.tsx
+++ pages/500.tsx
@@ -1,5 +1,9 @@
 import React from "react"
 import ErrorPage from "../src/error-page"
 
+export const config = {
+    unstable_runtimeJS: false
+};
+
 const FiveHundred = () => <ErrorPage errorCode={500} />
 export default FiveHundred
