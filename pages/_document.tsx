d3ff5021d4304553b522db6406bf9d800ec31e1c
cindyhont <cindyhont@gmail.com>
2023-01-13 14:17:53 +0000

    .

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,13 +6,13 @@ const Document = () => {
         <Html lang="en">
             <Head>
                 <script async src="https://www.googletagmanager.com/gtag/js?id=G-KKHR264QYG"></script>
-                <script>{`
+                <script dangerouslySetInnerHTML={{__html:`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
     
                     gtag('config', 'G-KKHR264QYG');
-                `}</script>
+                `}}></script>
                 <Script 
                     strategy='beforeInteractive' 
                     id='before-interactive'
